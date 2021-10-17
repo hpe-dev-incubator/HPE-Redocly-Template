d579c8a2324a907739a178eee75df9818d8f819e
Johnathan Giang <jayhgiang@gmail.com>
2021-10-16 23:34:20 -0700

    Remove old _components directory

--- _components/HoverCard/index.tsx
+++ _components/HoverCard/index.tsx
@@ -3,10 +3,6 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Card } from 'grommet';
 
-const StyledCard = styled(Card)`
-  transition: all 0.3s ease-in-out;
-`;
-
 export default function HoverCard({
   children,
   ...rest
@@ -14,7 +10,7 @@ export default function HoverCard({
   const [isFocused, setIsFocused] = React.useState(false);
 
   return (
-    <StyledCard
+    <Card
       background="background-front"
       elevation={isFocused ? 'medium' : 'small'}
       onBlur={() => setIsFocused(false)}
@@ -24,6 +20,6 @@ export default function HoverCard({
       {...rest}
     >
       {children}
-    </StyledCard>
+    </Card>
   );
 }
